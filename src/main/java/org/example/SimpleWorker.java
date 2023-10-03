00687fa455dd35979d72fbb5c9291b3168aac79f
kivanov <kivanov@siftscience.com>
2023-10-03 14:18:43 +0200

    ThreadPool executor examples added

---
+++ src/main/java/org/example/SimpleWorker.java
@@ -0,0 +1,47 @@
+package org.example;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
+
+public class SimpleWorker implements Runnable {
+    private final Socket clientSocket;
+    public SimpleWorker(Socket clientSocket) {
+        this.clientSocket = clientSocket;
+    }
+
+
+    @Override
+    public void run() {
+        processJob();
+    }
+
+    private void processJob() {
+        try(OutputStream output = clientSocket.getOutputStream()) {
+            StringBuilder response = new StringBuilder();
+            String responseBody = "Hello from thread [" + Thread.currentThread().getId() + "]";
+            addHeader(response, responseBody.length());
+            response.append(responseBody);
+            output.write(response.toString().getBytes());
+            output.flush();
+
+            clientSocket.close();
+            Thread.sleep(10000);
+            synchronized (this) {
+                notifyAll();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void addHeader(StringBuilder sb, int contentLength) {
+        sb.append("HTTP/1.1 200 OK\n")
+            .append("Content-type: plain/text\n")
+            .append("Content-length: ").append(contentLength)
+            .append("\n")
+            .append("\n");
+    }
+}
---
+++ src/main/java/org/example/ThreadPoolExecutorHttpServer.java
@@ -0,0 +1,47 @@
+package org.example;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ThreadPoolExecutorHttpServer {
+    private static final ExecutorService EXECUTOR_SERVICE = Executors.newFixedThreadPool(100);
+
+    public static void main(String[] args) {
+        new ThreadPoolExecutorHttpServer().start();
+    }
+
+    private void start() {
+        int port = 8080;
+
+        try {
+            // Create a server socket on port 8080
+            ServerSocket serverSocket = new ServerSocket(port);
+            System.out.println("Server listening on port " + port);
+            boolean first = true;
+
+            while (true) {
+                // Accept incoming client connections
+                Socket clientSocket = serverSocket.accept();
+                if (first) {
+                    clientSocket.close();
+                    first = false;
+                    continue;
+                } else {
+                    first = true;
+                }
+
+
+                System.out.println("Accepted connection from " + clientSocket.getInetAddress());
+                EXECUTOR_SERVICE.submit(new SimpleWorker(clientSocket));
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
+}
